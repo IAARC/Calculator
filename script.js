ae1714fe46b1cbbf3ce0b95229b9e5bcb7abe168
Andres Ramirez <gokutanve@gmail.com>
2022-12-29 00:16:42 -0400

    first version without design

---
+++ script.js
@@ -0,0 +1,72 @@
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operate');
+const clearBtn = document.querySelector('#clear');
+const resultBtn = document.querySelector('#result');
+const pastDisplay = document.querySelector('#pastDisplay');
+const display = document.querySelector('#display');
+
+let pastValue = '';
+let currentValue = "";
+let currentOperator = '';
+let result = null;
+
+function add(a,b){
+    return a + b;
+}
+function subtract(a,b){
+    return a- b;
+}
+function multiply(a,b){
+    return a * b;
+}
+function divide(a,b){
+    return a / b;
+}
+function operate(fn, a, b){
+    switch(fn){
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case 'x':
+            return multiply(a, b);
+        case '÷':
+            return divide(a, b);
+    }
+}
+function clear(){
+    let pastValue = '';
+    currentValue = "";
+    currentOperator = '';
+    result = null;
+    display.innerText = '0';
+    pastDisplay.innerText = '';
+}
+
+operators.forEach((operator) => operator.onclick = (e) =>{
+    currentOperator = e.target.value;
+    pastValue = currentValue;
+    currentValue = '';
+    if (pastValue === "") return pastDisplay.innerText = '0' + " " + currentOperator;
+    pastDisplay.innerText = pastValue + " " + currentOperator;
+})
+
+numbers.forEach((number) => number.onclick = (e) =>{
+    if (currentValue == result){
+    currentValue = ''
+    currentValue += e.target.value;
+    display.innerText = currentValue;
+} else {
+    currentValue += e.target.value;
+    display.innerText = currentValue;
+}
+})
+resultBtn.onclick = () =>{ 
+if (pastValue === '') return;
+result = operate(currentOperator,pastValue * 1, currentValue * 1);
+display.innerText = result;
+pastDisplay.innerText = pastValue + " " + currentOperator + " " + currentValue + "  =" ;
+currentValue = result;
+pastValue = '';
+}
+clearBtn.onclick = clear;
